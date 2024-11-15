bb54a062480e74e8c1387beb6a9e753d51ee480b
PLa <tanate.mea@ascendcorp.com>
2024-11-15 10:16:08 +0700

    chore: enhance script

--- src/jira/install.sh
+++ src/jira/install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Variables
 SCRIPT_URL="https://raw.githubusercontent.com/ascendcorp/userscripts/refs/heads/main/src/jira/generate-changelog.sh"
@@ -9,8 +9,10 @@ ALIAS_NAME="changelog"
 # Determine which shell configuration file to use
 if [ -f "$HOME/.zshrc" ]; then
     SHELL_RC="$HOME/.zshrc"
+    CURRENT_SHELL="zsh"
 else
     SHELL_RC="$HOME/.bashrc"
+    CURRENT_SHELL="bash"
 fi
 
 # Download the script
@@ -29,8 +31,12 @@ sudo mv $SCRIPT_NAME $INSTALL_DIR/$ALIAS_NAME
 echo "Adding alias to $SHELL_RC..."
 echo "alias $ALIAS_NAME='$INSTALL_DIR/$ALIAS_NAME'" >> $SHELL_RC
 
-# Reload shell configuration
-echo "Reloading shell configuration..."
-source $SHELL_RC
+# Reload shell configuration only if in the correct shell
+if [ "$SHELL" == "/bin/$CURRENT_SHELL" ]; then
+    echo "Reloading shell configuration..."
+    source $SHELL_RC
+else
+    echo "Please restart your terminal or run 'source $SHELL_RC' in your $CURRENT_SHELL shell to apply changes."
+fi
 
 echo "Installation complete. You can now run the script using the command: $ALIAS_NAME"
